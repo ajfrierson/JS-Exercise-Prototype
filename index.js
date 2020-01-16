90b9cae8ce1cbb9db4561b2576529930bc572b75
Alvin <alvin.frierson@fuller.edu>
2020-01-15 16:13:16 -0800

    completed the project with 24 tests passing and 3 tests failing.

--- index.js
+++ index.js
@@ -39,8 +39,25 @@ Airplane.prototype.land = function () {
         + It should return a string with `name` and `age`. Example: "Mary, 50"
 */
 
-function Person() {
+function Person(name, age) {
+  this.name = name;
+  this.age = age;
+  this.stomach = [];
+}
 
+Person.prototype.eat = function(someFood){
+  this.stomach.push(someFood);
+  if(this.stomach.length <= 10){
+    this.stomach.push(someFood);
+  }
+}
+
+Person.prototype.poop = function() {
+  this.stomach.length = 0;
+}
+
+Person.prototype.toString= function(){
+  return `${this.name}, ${this.age}`;
 }
 
 /*
@@ -57,8 +74,23 @@ function Person() {
         + The `drive` method should return a string "I ran out of fuel at x miles!" x being `odometer`.
 */
 
-function Car() {
+function Car(model, milesPerGallon) {
+  this.model = model;
+  this.milesPerGallon = milesPerGallon;
+  this.tank = 0;
+  this.odometer = 0;
+}
 
+Car.prototype.fill = function(gallons){
+  this.tank += gallons;
+}
+
+Car.prototype.drive = function(distance) {
+  this.odometer += distance;
+  this.tank -= (distance/this.milesPerGallon);
+  if(this.tank <= 0){
+    return `I ran out of fuel at ${this.odometer-1} miles!`;
+  }
 }
 
 /*
@@ -68,18 +100,25 @@ function Car() {
     - Besides the methods on Person.prototype, babies have the ability to `.play()`:
         + Should return a string "Playing with x", x being the favorite toy.
 */
-function Baby() {
+function Baby(name, age, favoriteToy) {
+  this.name = name;
+  this.age = age;
+  this.favoriteToy = favoriteToy;
+}
 
+Baby.prototype = Object.create(Person.prototype);
+Baby.prototype.play = function(){
+  return `Playing with ${this.favoriteToy}.`
 }
 
 /* 
   TASK 4
 
   In your own words explain the four principles for the "this" keyword below:
-  1. 
-  2. 
-  3. 
-  4. 
+  1. Implicit binding - this refers to the object to the left of the dot
+  2. Explicit binding - whenever using apply or call, this is explicitly stated
+  3. Window binding - this refers to the window/console object
+  4. New binding - whenever using a constructor this refers to the object created and returned by the constructor function
 */
 
 
